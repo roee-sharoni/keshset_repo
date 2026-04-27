0f6ddbc16ffc1c5f77a1cb59c0a65d00848ee818
roee-sharoni <roee@dataguild.pro>
2026-04-27 11:45:29 +0300

    Update lala.py

--- dags/lala.py
+++ dags/lala.py
@@ -1,1 +1,16 @@
+from airflow.sdk import DAG
+from airflow.providers.standard.operators.python import PythonOperator
+from datetime import datetime
 
+with DAG(
+    dag_id="example_dag",
+    start_date=datetime(2024, 1, 1),
+    schedule=None,
+    catchup=False,
+    default_args={"owner": "data-guild"},
+) as dag:
+
+    def hello():
+        print("Airflow 3 is running")
+
+    PythonOperator(task_id="hello", python_callable=hello)
